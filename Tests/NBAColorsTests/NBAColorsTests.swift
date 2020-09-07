3838e93e8fc1a58f26fb8afde17c7beb3944661e
Matthew Ramsden <ramsden.matthew@gmail.com>
2020-09-06 19:21:20 -0500

    tests: stub out unit test

--- Tests/NBAColorsTests/NBAColorsTests.swift
+++ Tests/NBAColorsTests/NBAColorsTests.swift
@@ -1,12 +1,15 @@
 import XCTest
 @testable import NBAColors
-
+import SwiftUI
 final class NBAColorsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(NBAColors().text, "Hello, World!")
+        XCTAssertEqual(Image(name, bundle: Bundle.module), Image(name, bundle: Bundle.module))
+        XCTAssertEqual(Logo("Chicago Bulls logo"), Logo("Chicago Bulls logo"))
+
     }
 
     static var allTests = [
